17cf15bcd497f74332ac4315831f3c412024e556
Tobby Lie <tobbylie@gmail.com>
2020-01-25 00:22:59 -0700

    adding hw2.py

---
+++ hw2.py
@@ -0,0 +1,21 @@
+from math import *
+
+k_constant = 9E9
+
+# 13.47
+
+# a & b
+electric_field(q, x, y):
+    # k*(q/(r^2))
+    # μC = 1E-6C
+    q = q*(1E6)
+    r = sqrt(x**2 + y**2)
+
+
+# 13.49
+
+# 15.22
+
+# 15.28
+
+# 15.26
